16723f9d42aa6be767dbd2fe0c587d01dec68f0d
DusanStevic <stevicdule@gmail.com>
2021-08-12 18:07:32 +0200

    Converting spring boot specific tools for service discovery and API gateway (Eureka and Spring Cloud Gateway) to general tools for service discovery and API gateway (Docker DNS and NGINX

--- AgentReportService/src/main/java/com/agent/report/ReportApplication.java
+++ AgentReportService/src/main/java/com/agent/report/ReportApplication.java
@@ -2,12 +2,10 @@ package com.agent.report;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableEurekaClient
 public class ReportApplication {
 
 	public static void main(String[] args) {
