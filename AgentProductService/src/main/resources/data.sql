7d5d9e88f422c62b8cdb1534601817f7ac3307a5
DusanStevic <stevicdule@gmail.com>
2021-08-18 19:27:59 +0200

    data.sql Populating Database

--- AgentProductService/src/main/resources/data.sql
+++ AgentProductService/src/main/resources/data.sql
@@ -1,2 +1,3 @@
-INSERT INTO PRODUCT(id, available, image_path, name, price) VALUES (100, 5, '123', 'ProductName', 50);
-INSERT INTO PRODUCT(id, available, image_path, name, price) VALUES (101, 5, '123', 'ProductName2', 50);
+INSERT INTO product(id, available, image_path, name, price) VALUES (100, 5, '123', 'ProductName', 50);
+INSERT INTO product(id, available, image_path, name, price) VALUES (101, 5, '123', 'ProductName2', 50);
+
