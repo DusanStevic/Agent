c3e68710a1fb1a3fadfc028aa072716744144bc9
DusanStevic <stevicdule@gmail.com>
2021-09-01 17:32:24 +0200

    docker skripta15

--- buildImages.sh
+++ buildImages.sh
@@ -22,7 +22,8 @@ DOCKER_BUILDKIT=1 docker build -t ${PRODUCTS} --no-cache .
 #DOCKER_BUILDKIT=1 docker build -t ${REPORTS} --no-cache .
 
 
-docker login --username ${DOCKERHUB_USERNAME} --password=${DOCKERHUB_PASSWORD}
+#docker login --username ${DOCKERHUB_USERNAME} --password=${DOCKERHUB_PASSWORD}
+docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_PASSWORD }
 #docker push ${GATEWAY}
 docker push ${PRODUCTS}
 #docker push ${PURCHASES}
