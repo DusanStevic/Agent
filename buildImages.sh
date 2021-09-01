6995e9d332da20f291cae4b81f383cf7685419f2
DusanStevic <stevicdule@gmail.com>
2021-09-01 19:46:24 +0200

    docker skripta22

--- buildImages.sh
+++ buildImages.sh
@@ -5,16 +5,16 @@ DOCKERHUB_USERNAME=${2}
 DOCKERHUB_PASSWORD=${3}
 
 
-PRODUCTS=${DOCKERHUB_USERNAME}/agent-product-service:${VERSION}
-REPORTS=${DOCKERHUB_USERNAME}/agent-report-service:${VERSION}
+PRODUCT=${DOCKERHUB_USERNAME}/agent-product-service:${VERSION}
+REPORT=${DOCKERHUB_USERNAME}/agent-report-service:${VERSION}
 SHOP=${DOCKERHUB_USERNAME}/agent-shop-service:${VERSION}
 #GATEWAY=${DOCKERHUB_USERNAME}/gateway:${VERSION}
 
 cd AgentProductService
-DOCKER_BUILDKIT=1 docker build -t ${PRODUCTS} --no-cache .
+DOCKER_BUILDKIT=1 docker build -t ${PRODUCT} --no-cache .
 
 cd ../AgentReportService
-DOCKER_BUILDKIT=1 docker build -t ${REPORTS} --no-cache .
+DOCKER_BUILDKIT=1 docker build -t ${REPORT} --no-cache .
 
 cd ../AgentShopService
 DOCKER_BUILDKIT=1 docker build -t ${SHOP} --no-cache .
@@ -23,7 +23,7 @@ DOCKER_BUILDKIT=1 docker build -t ${SHOP} --no-cache .
 
 docker login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD}
 
-docker push ${PRODUCTS}
-docker push ${REPORTS}
+docker push ${PRODUCT}
+docker push ${REPORT}
 docker push ${SHOP}
 #docker push ${GATEWAY}
