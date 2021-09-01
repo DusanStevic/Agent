31982baa6bcd53cfbe2c26b1b63d5d117b7ff593
DusanStevic <stevicdule@gmail.com>
2021-09-01 14:11:25 +0200

    docker skripta

---
+++ buildImages.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+VERSION=${1}
+DOCKERHUB_USERNAME=${2}
+DOCKERHUB_PASSWORD=${3}
+
+
+PRODUCTS=${DOCKERHUB_USERNAME}/agent-product-service:${VERSION}
+REPORTS=${DOCKERHUB_USERNAME}/agent-report-service:${VERSION}
+PURCHASES=${DOCKERHUB_USERNAME}/agent-shop-service:${VERSION}
+#GATEWAY=${DOCKERHUB_USERNAME}/gateway:${VERSION}
+
+cd AgentProductService
+DOCKER_BUILDKIT=1 docker build -t ${PRODUCTS} --no-cache .
+
+#cd ../reports
+#DOCKER_BUILDKIT=1 docker build -t ${REPORTS} --no-cache .
+
+#cd ../purchases
+#DOCKER_BUILDKIT=1 docker build -t ${PURCHASES} --no-cache .
+
+#DOCKER_BUILDKIT=1 docker build -t ${GATEWAY} --no-cache -f ./gateway/Dockerfile .
+
+docker login --username ${DOCKERHUB_USERNAME} --password=${DOCKERHUB_PASSWORD}
+
+docker push ${PRODUCTS}
+#docker push ${REPORTS}
+#docker push ${PURCHASES}
+#docker push ${GATEWAY}
