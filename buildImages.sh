d144f5ffb3025a031f43ffa5dca7d38ea00a9198
DusanStevic <stevicdule@gmail.com>
2021-09-01 19:36:31 +0200

    docker skripta21

--- buildImages.sh
+++ buildImages.sh
@@ -7,23 +7,23 @@ DOCKERHUB_PASSWORD=${3}
 
 PRODUCTS=${DOCKERHUB_USERNAME}/agent-product-service:${VERSION}
 REPORTS=${DOCKERHUB_USERNAME}/agent-report-service:${VERSION}
-PURCHASES=${DOCKERHUB_USERNAME}/agent-shop-service:${VERSION}
+SHOP=${DOCKERHUB_USERNAME}/agent-shop-service:${VERSION}
 #GATEWAY=${DOCKERHUB_USERNAME}/gateway:${VERSION}
 
 cd AgentProductService
 DOCKER_BUILDKIT=1 docker build -t ${PRODUCTS} --no-cache .
 
-#cd ../reports
-#DOCKER_BUILDKIT=1 docker build -t ${REPORTS} --no-cache .
+cd ../AgentReportService
+DOCKER_BUILDKIT=1 docker build -t ${REPORTS} --no-cache .
 
-#cd ../purchases
-#DOCKER_BUILDKIT=1 docker build -t ${PURCHASES} --no-cache .
+cd ../AgentShopService
+DOCKER_BUILDKIT=1 docker build -t ${SHOP} --no-cache .
 
 #DOCKER_BUILDKIT=1 docker build -t ${GATEWAY} --no-cache -f ./gateway/Dockerfile .
 
 docker login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD}
 
 docker push ${PRODUCTS}
-#docker push ${REPORTS}
-#docker push ${PURCHASES}
+docker push ${REPORTS}
+docker push ${SHOP}
 #docker push ${GATEWAY}
