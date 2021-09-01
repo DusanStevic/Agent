beba82517ba49718350c0a3fa85d95a1cadfbce7
DusanStevic <stevicdule@gmail.com>
2021-09-01 19:23:19 +0200

    docker skripta20

--- buildImages.sh
+++ buildImages.sh
@@ -5,27 +5,25 @@ DOCKERHUB_USERNAME=${2}
 DOCKERHUB_PASSWORD=${3}
 
 
+PRODUCTS=${DOCKERHUB_USERNAME}/agent-product-service:${VERSION}
+REPORTS=${DOCKERHUB_USERNAME}/agent-report-service:${VERSION}
+PURCHASES=${DOCKERHUB_USERNAME}/agent-shop-service:${VERSION}
 #GATEWAY=${DOCKERHUB_USERNAME}/gateway:${VERSION}
-PRODUCTS=${DOCKERHUB_USERNAME}/products:${VERSION}
-#PURCHASES=${DOCKERHUB_USERNAME}/purchases:${VERSION}
-#REPORTS=${DOCKERHUB_USERNAME}/reports:${VERSION}
-
-#DOCKER_BUILDKIT=1 docker build -t ${GATEWAY} --no-cache -f ./gateway/Dockerfile .
 
 cd AgentProductService
 DOCKER_BUILDKIT=1 docker build -t ${PRODUCTS} --no-cache .
 
-#cd ../purchases
-#DOCKER_BUILDKIT=1 docker build -t ${PURCHASES} --no-cache .
-
 #cd ../reports
 #DOCKER_BUILDKIT=1 docker build -t ${REPORTS} --no-cache .
 
+#cd ../purchases
+#DOCKER_BUILDKIT=1 docker build -t ${PURCHASES} --no-cache .
 
-#docker login --username ${DOCKERHUB_USERNAME} --password=${DOCKERHUB_PASSWORD}
-docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_PASSWORD}
-#docker login -u ${2} -p ${3}
-#docker push ${GATEWAY}
+#DOCKER_BUILDKIT=1 docker build -t ${GATEWAY} --no-cache -f ./gateway/Dockerfile .
+
+docker login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD}
+
 docker push ${PRODUCTS}
-#docker push ${PURCHASES}
 #docker push ${REPORTS}
+#docker push ${PURCHASES}
+#docker push ${GATEWAY}
