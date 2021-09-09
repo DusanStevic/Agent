026d6c31a6ea2a000957a9b5f2cfc8c5acfae7f1
DusanStevic <stevicdule@gmail.com>
2021-09-09 10:34:24 +0200

    main.tf folder terraform

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -6,8 +6,20 @@ terraform {
 provider "heroku" {
 }
 
-variable "servers_app_name" {
-  description = "Unique name of the Servers app"
+variable "product_app_name" {
+  description = "Unique name of the Product app"
+}
+
+variable "shopping_app_name" {
+  description = "Unique name of the Shopping app"
+}
+
+variable "report_app_name" {
+  description = "Unique name of the Report app"
+}
+
+variable "gateway_app_name" {
+  description = "Unique name of the Gateway app"
 }
 
 resource "heroku_config" "prod" {
@@ -16,60 +28,110 @@ resource "heroku_config" "prod" {
   }
 }
 
-resource "heroku_app" "servers1" {
-  name   = var.servers_app_name
+resource "heroku_app" "product-service" {
+  name   = var.product_app_name
   region = "eu"
   stack  = "container"
 }
 
-resource "heroku_build" "servers1" {
-  app = heroku_app.servers1.id
+resource "heroku_build" "product-service" {
+  app = heroku_app.product-service.id
 
   source {
-    path = "servers"
+    path = "products"
   }
 }
 
-resource "heroku_app_config_association" "servers1" {
-  app_id = heroku_app.servers1.id
+resource "heroku_app_config_association" "product-service" {
+  app_id = heroku_app.product-service.id
 
   vars = heroku_config.prod.vars
 }
 
 resource "heroku_addon" "database" {
-  app  = heroku_app.servers1.name
+  app  = heroku_app.product-service.name
   plan = "heroku-postgresql:hobby-dev"
 }
 
-resource "heroku_app" "servers2" {
-  name   = "${var.servers_app_name}-2"
+resource "heroku_app" "shopping-service" {
+  name   = var.shopping_app_name
   region = "eu"
   stack  = "container"
 }
 
-resource "heroku_build" "servers2" {
-  app = heroku_app.servers2.id
+resource "heroku_build" "shopping-service" {
+  app = heroku_app.shopping-service.id
 
   source {
-    path = "servers"
+    path = "shopping"
   }
 }
 
-resource "heroku_app_config_association" "servers2" {
-  app_id = heroku_app.servers2.id
+resource "heroku_app_config_association" "shopping-service" {
+  app_id = heroku_app.shopping-service.id
 
   vars = heroku_config.prod.vars
 }
 
 resource "heroku_addon_attachment" "database" {
-  app_id  = heroku_app.servers2.id
+  app_id  = heroku_app.shopping-service.id
   addon_id = heroku_addon.database.id
 }
 
-
-output "servers1_app_url" {
-  value = "https://${heroku_app.servers1.name}.herokuapp.com"
+resource "heroku_app" "report-service" {
+  name   = var.report_app_name
+  region = "eu"
+  stack  = "container"
 }
-output "servers2_app_url" {
-  value = "https://${heroku_app.servers2.name}.herokuapp.com"
+
+resource "heroku_build" "report-service" {
+  app = heroku_app.report-service.id
+
+  source {
+    path = "report"
+  }
+}
+
+resource "heroku_app_config_association" "report-service" {
+  app_id = heroku_app.report-service.id
+
+  vars = heroku_config.prod.vars
+}
+
+resource "heroku_addon_attachment" "database1" {
+  app_id  = heroku_app.report-service.id
+  addon_id = heroku_addon.database.id
+}
+
+resource "heroku_app" "gateway-service" {
+  name   = var.gateway_app_name
+  region = "eu"
+  stack  = "container"
+}
+
+resource "heroku_build" "gateway-service" {
+  app = heroku_app.gateway-service.id
+
+  source {
+    path = "gateway"
+  }
+}
+
+resource "heroku_app_config_association" "gateway-service" {
+  app_id = heroku_app.gateway-service.id
+
+  vars = heroku_config.prod.vars
+}
+
+output "product_app_url" {
+  value = "http://${heroku_app.product-service.name}.herokuapp.com"
+}
+output "shopping_app_url" {
+  value = "http://${heroku_app.shopping-service.name}.herokuapp.com"
+}
+output "report_app_url" {
+  value = "http://${heroku_app.report-service.name}.herokuapp.com"
+}
+output "gateway_app_url" {
+  value = "http://${heroku_app.gateway-service.name}.herokuapp.com"
 }
