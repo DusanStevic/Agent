a349f49626f4d0b8c7dbbda8cd545659c4d7333d
DusanStevic <stevicdule@gmail.com>
2021-09-11 20:22:19 +0200

    subota 20:25

---
+++ AgentProductService/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO product(id, available, image_path, name, price) VALUES (100, 5, '123', 'ProductName', 50);
+INSERT INTO product(id, available, image_path, name, price) VALUES (101, 5, '123', 'ProductName2', 50);
+
