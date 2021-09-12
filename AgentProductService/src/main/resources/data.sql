78938bb75cdfddb1da09856f284956b106385e2a
DusanStevic <stevicdule@gmail.com>
2021-09-12 11:20:27 +0200

    nedelja 11:20

--- AgentProductService/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO product(id, available, image_path, name, price) VALUES (100, 5, '123', 'ProductName', 50);
-INSERT INTO product(id, available, image_path, name, price) VALUES (101, 5, '123', 'ProductName2', 50);
-
--- AgentShopService/src/main/resources/data.sql
+++ AgentShopService/src/main/resources/data.sql
@@ -1,1 +1,3 @@
 INSERT INTO agent(id, password, username) VALUES (102, '$2a$10$pgZHC62DAB45ixdyh0ugu.zy3jAhLan8gz5x57b8yYiGRkxEA2pmu', 'admin');
+INSERT INTO product(id, available, image_path, name, price) VALUES (100, 5, '123', 'ProductName', 50);
+INSERT INTO product(id, available, image_path, name, price) VALUES (101, 5, '123', 'ProductName2', 50);
